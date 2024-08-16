c0168c3780f9b360bc2307681bae352c6e24886b
vojtechpavlu <vojtech.pavlu.dev@gmail.com>
2024-08-16 21:31:10 +0200

    Add seeded randomizer

---
+++ src/randomizer/seeded.ts
@@ -0,0 +1,24 @@
+import { RandomizerConfiguration, RandomizerFactory } from './base';
+
+/** Simple configuration for seeded randomizer */
+export interface SeededRandomizerConfiguration extends RandomizerConfiguration {
+  seed: number;
+}
+
+/**
+ * Generates a seeded (deterministic) pseudo-randomizer. This implementation
+ * follows Linear congruential generator algorithm.
+ *
+ * @param {SeededRandomizerConfiguration} config Configuration specifying
+ * the seed to be used by the deterministic randomizer.
+ */
+export const seededRandomizer: RandomizerFactory<SeededRandomizerConfiguration> = (
+  config?: SeededRandomizerConfiguration
+) => {
+  let { seed } = config ?? { seed: 1 };
+
+  return () => {
+    seed = (seed * 9301 + 49297) % 233280;
+    return seed / 233280;
+  };
+};
