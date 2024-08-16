d5cbc2175f8f525de4034d59e3bc8bec0ab8cbaa
vojtechpavlu <vojtech.pavlu.dev@gmail.com>
2024-08-16 20:30:56 +0200

    Update eslint settings

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
+/**
+ * Sum function
+ * @param {number} a first number
+ * @param {number} b second number
+ *
+ * @returns {number} sum of the two numbers
+ */
 export const sum = (a: number, b: number): number => {
   return a + b;
 };
