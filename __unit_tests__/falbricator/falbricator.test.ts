69d8f8ce162b710325ad27510ddbe629d8d725a7
vojtechpavlu <vojtech.pavlu.dev@gmail.com>
2024-08-23 00:24:48 +0200

    Add test for schema compilation

---
+++ __unit_tests__/falbricator/falbricator.test.ts
@@ -0,0 +1,50 @@
+import { compileSchemaInput, Ecosystem, Schema, SchemaInput } from '../../src';
+import { generateFalsum } from '../../src/falbricator';
+
+const schemaInput: SchemaInput = {
+  randomizer: {
+    name: 'constant',
+    config: {
+      value: 0.5,
+    },
+  },
+  fields: {
+    age1: {
+      type: 'integer',
+      config: {
+        max: 100,
+      },
+    },
+    age2: {
+      type: 'integer',
+      config: {
+        max: 10,
+      },
+    },
+  },
+};
+
+const schema: Schema = compileSchemaInput(schemaInput, new Ecosystem());
+
+describe('generateFalsum function', () => {
+  it('should generate a falsum of an expected shape', () => {
+    const falsum = generateFalsum(
+      schema,
+      schema.randomizerFactory(schema.randomizerConfig),
+    );
+
+    expect(Object.keys(falsum).length).toBe(2);
+    expect(typeof falsum.age1).toBe('number');
+    expect(typeof falsum.age2).toBe('number');
+  });
+
+  it('should generate expected values', () => {
+    const falsum = generateFalsum(
+      schema,
+      schema.randomizerFactory(schema.randomizerConfig),
+    );
+
+    expect(falsum.age1).toBe(50);
+    expect(falsum.age2).toBe(5);
+  });
+});
