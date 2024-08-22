77e6498df1ddc3df13fedb76abadded04930b28f
vojtechpavlu <vojtech.pavlu.dev@gmail.com>
2024-08-22 23:59:53 +0200

    Use TypeError instead

--- src/randomizer/constant.ts
+++ src/randomizer/constant.ts
@@ -8,7 +8,7 @@ import { Randomizer, RandomizerConfiguration, RandomizerFactory } from './base';
  * @returns {Randomizer} Constant randomizer returning the preconfigured value
  *
  * @throws {Error} When the `value` is not present within configuration
- * @throws {Error} When the `value` is not a number
+ * @throws {TypeError} When the `value` is not a number
  * @throws {Error} When the `value` is not within range `[0, 1]`
  */
 export const constantRandomizer: RandomizerFactory = (
@@ -19,7 +19,7 @@ export const constantRandomizer: RandomizerFactory = (
       `Constant value must be specified within the given configuration for the constant randomizer`
     );
   } else if (typeof config.value !== 'number') {
-    throw new Error(
+    throw new TypeError(
       `Constant value must be a number for constant randomizer (${typeof config.value})`
     )
   } else if (config.value < 0 || config.value > 1) {
