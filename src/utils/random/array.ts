9cd42ce7e828be77ce44080282bd122209b526dd
vojtechpavlu <vojtech.pavlu.dev@gmail.com>
2024-08-25 20:15:35 +0200

    Add JSDocs

--- src/utils/random/array.ts
+++ src/utils/random/array.ts
@@ -1,6 +1,20 @@
 import { Randomizer } from '../../randomizer';
 import { randomInteger } from './number';
 
+/**
+ * Selects a single item from the given array.
+ *
+ * @param {Randomizer} randomizer Randomizer to be used for picking an item from the array
+ * @param {T[]} array Array of items this function selects a single item
+ *
+ * @returns {T} A single item taken from the given array.
+ *
+ * @throws {Error} When the given array is an empty value
+ * @throws {TypeError} When the given array is not of type Array
+ * @throws {Error} When the given array is empty
+ *
+ * @template T Type of items the array consists of
+ */
 export const pickRandomItem = <T>(randomizer: Randomizer, array: T[]): T => {
   if (!array && !Array.isArray(array))
     throw new Error(`Can't pick an item - got empty value instead of array`);
