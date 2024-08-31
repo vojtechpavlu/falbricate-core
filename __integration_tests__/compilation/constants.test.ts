c2cebfcf193820f3474b7d082af1ff88d5c70be9
vojtechpavlu <79374331+vojtechpavlu@users.noreply.github.com>
2024-08-31 19:25:12 +0200

    Add constant standard (#27)

---
+++ __integration_tests__/compilation/constants.test.ts
@@ -0,0 +1,71 @@
+import { Ecosystem, SchemaInput } from '../../src';
+
+describe('Constant standards', () => {
+  it('should use a string constant standard', () => {
+    const ecosystem = new Ecosystem();
+
+    const schema: SchemaInput = {
+      fields: {
+        val: '!const-test-string',
+      },
+    };
+
+    const falbricate = ecosystem.compile(schema);
+    const item = falbricate.generate();
+
+    expect(item.val).toBe('test-string');
+  });
+
+  it('should use a number constant standard (parses it)', () => {
+    const ecosystem = new Ecosystem();
+
+    const schema: SchemaInput = {
+      fields: {
+        val: '!const-123',
+      },
+    };
+
+    const falbricate = ecosystem.compile(schema);
+    const item = falbricate.generate();
+
+    expect(item.val).toBe(123);
+  });
+
+  it('should use an object constant standard (parses it)', () => {
+    const ecosystem = new Ecosystem();
+
+    const schema: SchemaInput = {
+      fields: {
+        val: '!const-{"k": "v"}',
+      },
+    };
+
+    const falbricate = ecosystem.compile(schema);
+    const item = falbricate.generate();
+
+    // @ts-expect-error Ignore the error
+    expect(item.val.k).toBe('v');
+  });
+
+  it('should use an array constant standard (parses it)', () => {
+    const ecosystem = new Ecosystem();
+
+    const schema: SchemaInput = {
+      fields: {
+        val: '!const-["item1", "item2", 3]',
+      },
+    };
+
+    const falbricate = ecosystem.compile(schema);
+    const item = falbricate.generate();
+
+    // @ts-expect-error Ignore the error
+    expect(item.val[0]).toBe('item1');
+
+    // @ts-expect-error Ignore the error
+    expect(item.val[1]).toBe('item2');
+
+    // @ts-expect-error Ignore the error
+    expect(item.val[2]).toBe(3);
+  });
+});
--- src/schema/schema.ts
+++ src/schema/schema.ts
@@ -8,6 +8,7 @@ import { Ecosystem } from '../ecosystem';
 import { RandomizerFactory } from '../randomizer';
 
 const REFERENCE_PREFIX = '!ref-';
+const CONSTANT_PREFIX = '!const-';
 
 export interface SchemaInput {
   randomizer?: {
@@ -36,20 +37,39 @@ const compileRandomizerFactory = (
     : ecosystem.getDefaultRandomizer();
 };
 
+const compileStandard = (
+  ecosystem: Ecosystem,
+  field: string,
+): ValueGenerator => {
+  if (field.startsWith(REFERENCE_PREFIX)) {
+    const path = field.slice(REFERENCE_PREFIX.length);
+    const factory = ecosystem.getValueGeneratorFactory('reference');
+    return factory({ ecosystem, path });
+  } else if (field.startsWith(CONSTANT_PREFIX)) {
+    let value: unknown = field.slice(CONSTANT_PREFIX.length);
+
+    try {
+      // Try to parse the value; might not be possible
+      value = JSON.parse(value as string);
+    } catch {
+      /* intentionally empty */
+    }
+
+    const factory = ecosystem.getValueGeneratorFactory('constant');
+    return factory({ ecosystem, value });
+  }
+
+  const valueGeneratorFactory = ecosystem.getValueGeneratorFactory(field);
+  return valueGeneratorFactory({ ecosystem });
+};
+
 const compileFieldDefinition = (
   ecosystem: Ecosystem,
   field: FieldDefinition,
   randomizerFactory: RandomizerFactory,
 ): ValueGenerator => {
   if (typeof field === 'string') {
-    if (field.startsWith(REFERENCE_PREFIX)) {
-      const path = field.slice(REFERENCE_PREFIX.length);
-      const factory = ecosystem.getValueGeneratorFactory('reference');
-      return factory({ ecosystem, path });
-    }
-
-    const valueGeneratorFactory = ecosystem.getValueGeneratorFactory(field);
-    return valueGeneratorFactory({ ecosystem });
+    return compileStandard(ecosystem, field);
   } else if (typeof field === 'object') {
     const valueGeneratorFactory = ecosystem.getValueGeneratorFactory(
       field.type,
