58bf8b73b0ca52b5812c63c1e2cbbb229fb64610
vojtechpavlu <vojtech.pavlu.dev@gmail.com>
2024-08-31 18:03:23 +0200

    Add integration tests

---
+++ __integration_tests__/compilation/standards.test.ts
@@ -0,0 +1,20 @@
+import { Ecosystem, SchemaInput } from '../../src';
+
+describe('Standards', () => {
+  it('should be able to deal with standard value generators', () => {
+    const ecosystem = new Ecosystem();
+
+    const schema: SchemaInput = {
+      fields: {
+        testTrue: 'true',
+        testFalse: 'false',
+      },
+    };
+
+    const falbricate = ecosystem.compile(schema);
+    const generated = falbricate.generate();
+
+    expect(generated.testTrue).toBe(true);
+    expect(generated.testFalse).toBe(false);
+  });
+});
