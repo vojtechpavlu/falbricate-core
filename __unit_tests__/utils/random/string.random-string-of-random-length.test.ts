5d847fb74178f045efb59f214a564f7d1719e739
vojtechpavlu <vojtech.pavlu.dev@gmail.com>
2024-09-01 12:28:05 +0200

    Cleanup

--- __unit_tests__/utils/random/string.random-string-of-random-length.test.ts
+++ __unit_tests__/utils/random/string.random-string-of-random-length.test.ts
@@ -1,27 +1,21 @@
-import { randomString, randomStringOfRandomLength } from '../../../src';
+import { randomStringOfRandomLength } from '../../../src';
 
 const randomizer = Math.random;
 const charset = ['a', 'b'];
 
 describe('randomStringOfRandomLength function', () => {
   it('should generate a string of length within the interval', () => {
-    const generated = randomStringOfRandomLength(
-      randomizer,
-      charset,
-      10,
-      1
-    );
+    const generated = randomStringOfRandomLength(randomizer, charset, 10, 1);
 
     expect(generated.length).toBeGreaterThanOrEqual(1);
     expect(generated.length).toBeLessThanOrEqual(10);
   });
 
   it('should throw on maxLength < minLength', () => {
-    expect(() => randomStringOfRandomLength(
-      randomizer,
-      charset,
-      1,
-      10
-    )).toThrow(`Can't generate a random string of random length when maxLength < minLength`);
+    expect(() =>
+      randomStringOfRandomLength(randomizer, charset, 1, 10),
+    ).toThrow(
+      `Can't generate a random string of random length when maxLength < minLength`,
+    );
   });
 });
