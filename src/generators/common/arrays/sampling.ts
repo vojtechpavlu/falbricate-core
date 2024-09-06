e5fbe8299b1a983110c2562806eb067c9cf39ad3
vojtechpavlu <vojtech.pavlu.dev@gmail.com>
2024-09-06 23:48:34 +0200

    Rename props

--- src/generators/common/arrays/sampling.ts
+++ src/generators/common/arrays/sampling.ts
@@ -19,22 +19,22 @@ export const samplingGenerator: ValueGeneratorFactory = (
     throw new Error(`Can't make a sample - the given array is empty`);
   }
 
-  const min = (config.min as number) ?? 0;
-  const max = (config.max as number) ?? array.length;
+  const minLength = (config.minLength as number) ?? 0;
+  const maxLength = (config.maxLength as number) ?? array.length;
 
-  if (!max && max !== 0) {
-    throw new Error(`Can't make a sample - 'max' is not specified`);
-  } else if (min < 0 || max < 0) {
+  if (!maxLength && maxLength !== 0) {
+    throw new Error(`Can't make a sample - 'maxLength' is not specified`);
+  } else if (minLength < 0 || maxLength < 0) {
     throw new Error(
-      `Can't make a sample - both 'min' and 'max' mustn't be negatives`,
+      `Can't make a sample - both 'minLength' and 'maxLength' mustn't be negatives`,
     );
-  } else if (min > max) {
+  } else if (minLength > maxLength) {
     throw new Error(
       `Can't make a sample - maximum sample length is less than the minimum`,
     );
   }
 
   return (context: GenerationContext) => {
-    return selectSample(context.randomizer, array, max, min);
+    return selectSample(context.randomizer, array, maxLength, minLength);
   };
 };
