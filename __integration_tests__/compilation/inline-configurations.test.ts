2c2f57758c38fd3ba941f8ade2e575026fe10024
vojtechpavlu <vojtech.pavlu.dev@gmail.com>
2024-09-13 19:13:13 +0200

    Add integration test

---
+++ __integration_tests__/compilation/inline-configurations.test.ts
@@ -0,0 +1,20 @@
+import { getDefaultEcosystem, SchemaInput } from '../../src';
+
+describe('Inline configurations', () => {
+  it('should accept inline configurations', () => {
+    const ecosystem = getDefaultEcosystem();
+
+    const schema: SchemaInput = {
+      fields: {
+        integer: 'integer?min=15&max=20',
+      },
+    };
+
+    const falbricate = ecosystem.compile(schema);
+    const item = falbricate.generate();
+
+    expect(typeof item.integer).toBe('number');
+    expect(item.integer).toBeGreaterThanOrEqual(15);
+    expect(item.integer).toBeLessThanOrEqual(20);
+  });
+});
