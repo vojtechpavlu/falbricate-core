bb65c7b1558ef2fd9f2930031584e65ccb218d4d
vojtechpavlu <vojtech.pavlu.dev@gmail.com>
2024-09-15 10:20:22 +0200

    Update the packing system

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,28 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
 
 export default {
   input: 'src/index.ts',
   output: [
     {
-      file: 'dist/bundle.cjs.js',
-      format: 'cjs',
-      sourcemap: true,
-    },
-    {
-      file: 'dist/bundle.esm.js',
+      file: 'dist/bundle.esm.mjs',
       format: 'esm',
       sourcemap: true,
     },
+    {
+      file: 'dist/bundle.cjs.cjs',
+      format: 'cjs',
+      sourcemap: true,
+    },
   ],
-  plugins: [nodeResolve(), commonjs(), typescript()],
+  plugins: [
+    nodeResolve(),
+    commonjs(),
+    typescript({
+      declaration: true,
+      declarationDir: 'dist',
+      rootDir: 'src',
+    }),
+  ],
 };
