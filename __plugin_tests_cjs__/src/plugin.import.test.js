0c41a6e404ca9785959c661f98a042e7c8332d63
vojtechpavlu <vojtech.pavlu.dev@gmail.com>
2024-09-15 10:58:52 +0200

    Create CJS tests

---
+++ __plugin_tests_cjs__/src/plugin.import.test.js
@@ -0,0 +1,43 @@
+const { Ecosystem, getDefaultEcosystem } = require('@falbricate/core');
+
+describe('Importing a custom plugin into Ecosystem', () => {
+  it('should include the given plugin', () => {
+    const plugin = {
+      valueGenerators: {
+        'test-value-generator': () => {
+          return () => 'generated-test-value';
+        },
+      },
+    };
+
+    const ecosystem = new Ecosystem();
+    ecosystem.register(plugin);
+
+    expect(ecosystem.hasValueGeneratorFactory('test-value-generator')).toBe(
+      true,
+    );
+  });
+
+  it('should be able to reach the imported resources', () => {
+    const plugin = {
+      valueGenerators: {
+        'test-value-generator': () => {
+          return () => 'generated-test-value';
+        },
+      },
+    };
+
+    const ecosystem = getDefaultEcosystem();
+    ecosystem.register(plugin);
+
+    const schema = {
+      fields: {
+        testField: 'test-value-generator'
+      }
+    }
+
+    const falbricate = ecosystem.compile(schema);
+    const generated = falbricate.generate();
+    expect(generated.testField).toBe('generated-test-value');
+  });
+});
